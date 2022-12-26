520590440677ea4897c7132876eba7987e00d749
JimmyAlvess <matheusjjallves@gmail.com>
2022-12-26 16:15:07 -0300

    Criando o Controller a View para exbir os lanches

---
+++ LanchesMac/LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,22 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILanchesRepository _lanchesRepository;
+
+        public LancheController(ILanchesRepository lanchesRepository)
+        {
+            _lanchesRepository = lanchesRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lanchesRepository.Lanches;
+            return View(lanches); 
+        }
+    }
+}
