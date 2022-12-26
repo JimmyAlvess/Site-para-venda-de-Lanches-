5f816a3a76583f62642ea1e90038fdf7a455415c
JimmyAlvess <matheusjjallves@gmail.com>
2022-12-26 15:44:01 -0300

    Criando repositorios

---
+++ LanchesMac/LanchesMac/Repositories/LachesRepository.cs
@@ -0,0 +1,27 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+
+namespace LanchesMac.Repositories
+{
+    public class LachesRepository : ILanchesRepository
+    {
+        private readonly AppDbContext _context;
+
+        public LachesRepository(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+        public IEnumerable<Lanche> Lanches => _context.Lanches;
+
+        public IEnumerable<Lanche> LanchesPreferidos => _context.Lanches
+            .Where(p => p.IsLanchePreferido)
+            .Include(c => c.categoria);
+        public Lanche GetLancheById(int lancheid)
+        {
+          return  _context.Lanches.FirstOrDefault(l => l.LancheId == lancheid);
+        }
+    }
+}
