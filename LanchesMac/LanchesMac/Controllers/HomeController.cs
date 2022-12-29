9a6cbaf41465a3b3f9fc3a562a49a474baa92753
JimmyAlvess <matheusjjallves@gmail.com>
2022-12-29 12:35:38 -0300

    Exibindo os lanches preferidos

--- LanchesMac/LanchesMac/Controllers/HomeController.cs
+++ LanchesMac/LanchesMac/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,10 +8,21 @@ namespace LanchesMac.Controllers
 {
     public class HomeController : Controller
     {
-    
+
+        private readonly ILanchesRepository _lancheRepository;
+
+        public HomeController(ILanchesRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel
+            {
+                LanchesPreferido = _lancheRepository.LanchesPreferidos
+            };
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
